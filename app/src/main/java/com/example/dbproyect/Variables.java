e88094c387db1739d6c97906f59e6eca5115eaaf
Oliver Visoso <ovisoso@ucol.mx>
2022-11-04 14:18:45 -0600

    Elimine los cambios

--- app/src/main/java/com/example/dbproyect/Variables.java
+++ app/src/main/java/com/example/dbproyect/Variables.java
@@ -6,11 +6,6 @@ public class Variables {
     public static final String CAMPO_ID = "id";
     public static final String CAMPO_NOMBRE = "nombre";
     public static final String CAMPO_TELEFONO = "telefono";
-    public static final String CAMPO_APELLIDO = "apellido";
-    public static final String CAMPO_EDAD = "edad";
-    public static final String CAMPO_SEXO = "sexo";
-    public static final String CAMPO_FECHA = "fecha";
-    public static final String CAMPO_ESTATURA = "estatura";
     public static final String CREAR_TABLA = "CREATE TABLE "+NOMBRE_TABLA+
             "("+CAMPO_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+CAMPO_NOMBRE+ " TEXT, "+CAMPO_TELEFONO+ " TEXT)";
     public static final String ELIMINAR_TABLA = "DROP TABLE IF EXIST "+NOMBRE_TABLA;
